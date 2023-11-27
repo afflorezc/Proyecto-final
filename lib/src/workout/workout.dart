f09e2172ec40d70c8a47a914de982e82a343a5d2
Thomas Ciro <thomyciro@gmail.com>
2023-11-27 16:41:16 -0500

    modified checkout

--- lib/src/workout/workout.dart
+++ lib/src/workout/workout.dart
@@ -14,9 +14,15 @@ class Workout extends StatelessWidget{
         children: <Widget> [
           GridGraficos(),
           Padding(padding: EdgeInsets.all(10.0)),
-          GridGraficos(),
+          GridStats(),
           Padding(padding: EdgeInsets.all(10.0)),
-
+          Center(
+            child: Text("Tiempo: 00:00"),
+          ),
+          IconButton(
+            icon: Icon(Icons.play_circle),
+            onPressed: (){}, 
+          ),
         ],
       ),
     );
@@ -39,4 +45,35 @@ class GridGraficos extends StatelessWidget{
     
   }
 
+}
+
+class GridStats extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 2,
+
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.monitor_heart),
+          title: Text("120"),
+        ),
+
+        ListTile(
+          leading:Icon(Icons.local_fire_department),
+          title: Text("200 W"),
+        ),
+
+        ListTile(
+          leading:Icon(Icons.bike_scooter),
+          title: Text("92"),
+        ),
+
+        ListTile(
+          leading:Icon(Icons.incomplete_circle),
+          title: Text("30 km/h"),
+        ),
+      ],
+    );
+  }
 }
